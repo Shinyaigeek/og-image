676afbc4682b7035c4e8e896e00c8b70515de242
shinyaigeek <PhilisPaxil@gmail.com>
2021-01-07 18:13:47 +0900

    fix: remove font load unnecessalliry

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,66 +1,18 @@
-import { readFileSync } from "fs";
-// import marked from "marked";
 import { sanitizeHtml } from "./sanitizer";
 import { ParsedRequest } from "./types";
-// const twemoji = require("twemoji");
-// const twOptions = { folder: "svg", ext: ".svg" };
-// const emojify = (text: string) => twemoji.parse(text, twOptions);
+const twemoji = require("twemoji");
+const twOptions = { folder: "svg", ext: ".svg" };
+const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(
-  `${__dirname}/../_fonts/Inter-Regular.woff2`
-).toString("base64");
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString(
-  "base64"
-);
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
-  "base64"
-);
-
-function getCss(theme: string, fontSize: string) {
-  let background = "white";
-  let foreground = "black";
-  let radial = "lightgray";
-
-  if (theme === "dark") {
-    background = "black";
-    foreground = "white";
-    radial = "dimgray";
-  }
-  return `
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
-    }
-
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
-    }
-
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
+function getCss() {
+    return `
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@800');
 
     body {
-        height: 100vh;
         display: flex;
         text-align: center;
         align-items: center;
         justify-content: center;
-    }
-
-    code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
     }
 
     .frame {
@@ -85,6 +37,17 @@ function getCss(theme: string, fontSize: string) {
         justify-items: center;
     }
 
+    .title {
+        font-size: 96px;
+        font-weight: bold;
+        font-family: 'M PLUS Rounded 1c', sans-serif;
+        width: 70vw;
+        height: 70vh;
+        position: absolute;
+        top: calc(12vh + 72px);
+        left: calc(12vw + 72px);
+    }
+
     .logo {
         position: absolute;
         bottom: 24px;
@@ -106,15 +69,7 @@ function getCss(theme: string, fontSize: string) {
         width: 1em;
         margin: 0 .05em 0 .1em;
         vertical-align: -0.1em;
-    }
-    
-    .heading {
-        font-family: 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
-    }`;
+    }`
 }
 
 // const hinan = `
@@ -133,31 +88,33 @@ function getCss(theme: string, fontSize: string) {
 //         </div>`
 
 export function getHtml(parsedReq: ParsedRequest) {
-  const { theme, fontSize, title } = parsedReq;
-  return `<!DOCTYPE html>
+    const { title } = parsedReq;
+    return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        ${getCss(theme, fontSize)}
+        ${getCss()}
     </style>
     <body>
     <div class="frame"></div>
-    ${title}
+    <div class="title">
+    ${emojify(sanitizeHtml(title))}
+    </div>
     <div class="icon">
         ${getImage(
-          "https://static.shinyaigeek.dev/static/icon_transparent.png",
-          "auto",
-          "530"
-        )}
+        "https://static.shinyaigeek.dev/static/icon_transparent.png",
+        "auto",
+        "530"
+    )}
     </div>
     </body>
 </html>`;
 }
 
 function getImage(src: string, width = "auto", height = "225") {
-  return `<img
+    return `<img
         class="logo"
         alt="Generated Image"
         src="${sanitizeHtml(src)}"
@@ -165,7 +122,3 @@ function getImage(src: string, width = "auto", height = "225") {
         height="${sanitizeHtml(height)}"
     />`;
 }
-
-// function getPlusSign(i: number) {
-//   return i === 0 ? "" : '<div class="plus">+</div>';
-// }
