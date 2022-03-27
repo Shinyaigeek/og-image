a6d0051116e7e860a7296555fd85c9aa61657d0b
Shinyaigeek <Philispaxil@gmail.com>
2022-03-27 19:42:48 +0900

    Chore: add v2

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -18,7 +18,7 @@ export function parseRequest(req: IncomingMessage) {
     throw new Error("Expected a single title");
   }
 
-  const arr = (pathname || "/").slice(1).split(".");
+  const arr = (pathname || "/").slice(2).split(".");
   let extension = "";
   let text = "";
   if (arr.length === 0) {
@@ -39,7 +39,7 @@ export function parseRequest(req: IncomingMessage) {
     images: getArray(images),
     widths: getArray(widths),
     heights: getArray(heights),
-    title: title ?? "",
+    title: decodeURIComponent(title ?? ""),
   };
   parsedRequest.images = getDefaultImages(
     parsedRequest.images,
